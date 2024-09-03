8933a072a6f79341b5650d73afad1c5e49a32c40
Ryan Alves <ra546038@gmail.com>
2024-09-03 16:23:09 -0300

    feat: using api

--- app/src/main/java/br/senai/sp/jandira/rickyandmorty/screens/CharactersList.kt
+++ app/src/main/java/br/senai/sp/jandira/rickyandmorty/screens/CharactersList.kt
@@ -10,11 +10,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -23,10 +28,32 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.senai.sp.jandira.rickyandmorty.model.Character
+import br.senai.sp.jandira.rickyandmorty.model.Result
+import br.senai.sp.jandira.rickyandmorty.service.RetrofitFactory
 import coil.compose.AsyncImage
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 @Composable
 fun CharactersList() {
+
+    var characterList by remember {
+        mutableStateOf(listOf<Character>())
+    }
+
+    // Efetuar chamada para API
+    val callCharacterList = RetrofitFactory()
+        .getCharacterService().getAllCharacters()
+
+    callCharacterList.enqueue(object : Callback<Result>{
+        override fun onResponse(p0: Call<Result>, response: Response<Result>) {
+            characterList = response.body()!!.results
+        }
+        override fun onFailure(p0: Call<Result>, p1: Throwable) {
+        }
+    })
+
     Surface (
         modifier = Modifier
             .fillMaxSize(),
@@ -46,8 +73,8 @@ fun CharactersList() {
             )
             Spacer(modifier = Modifier.height(32.dp))
             LazyColumn {
-                items(5){
-                    CharacterCard(character = Character())
+                items(characterList){
+                    CharacterCard(character = it)
                 }
             }
 
@@ -73,8 +100,8 @@ fun CharacterCard(character: Character) {
                     .size(80.dp)
             ) {
                 AsyncImage(
-                    model = "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-                    contentDescription = ""
+                    model = character.image,
+                    contentDescription = character.name
                 )
             }
             Column (
@@ -84,11 +111,11 @@ fun CharacterCard(character: Character) {
                     .padding(start = 12.dp)
             ) {
                 Text(
-                    text = "Nome do personagem",
+                    text = character.name,
                     fontSize = 20.sp,
                     fontWeight = FontWeight.Bold
                 )
-                Text(text = "Espécie")
+                Text(text = character.species)
             }
         }
     }
